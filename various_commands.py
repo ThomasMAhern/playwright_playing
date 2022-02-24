47ac797540b1a2d9348687f9bb3f95d61a8e4bbe
ThomasMAhern <33540039+ThomasMAhern@users.noreply.github.com>
2022-02-24 11:21:19 +0100

    Update various_commands.py
    Added sec paperwork button clicking things

--- various_commands.py
+++ various_commands.py
@@ -12,3 +12,34 @@ await page.pause()
 
 
 
+
+
+#Automating clicking the no buttons for e-qip paperwork 
+#
+await page.click("p:has-text(\"No\")")
+await page.click("fieldset:nth-child(10) .eqip-block-error .usa-grid div:nth-child(2) .eqip-height-2 p")
+await page.click("fieldset:nth-child(12) .eqip-block-error .usa-grid div:nth-child(2) .eqip-height-2 p")
+await page.click("fieldset:nth-child(14) .eqip-block-error .usa-grid div:nth-child(2) .eqip-height-2 p")
+await page.click("fieldset:nth-child(16) .eqip-block-error .usa-grid div:nth-child(2) .eqip-height-2 p")
+await page.click("fieldset:nth-child(18) .eqip-block-error .usa-grid div:nth-child(2) .eqip-height-2 p")
+await page.click("fieldset:nth-child(20) .eqip-block-error .usa-grid div:nth-child(2) .eqip-height-2 p")
+await page.click("text=Save/Continue")
+#
+#
+
+
+
+#Automating clicking through saying relatives aren't deceased for e-qip paperwork 
+#
+await page.click("text=Relative Deceased Question")
+await page.click("text=Save")
+await page.click("text=Summary")
+#
+#
+
+
+
+
+
+await page.close()
+await playwright.stop()
